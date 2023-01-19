24c092936bdd75356acb601e28529731c56f43cd
Sampath Paravasthu <write2sampath@gmail.com>
2023-01-19 23:14:04 +0530

    added scalfold codes

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ class Tree {
  constructor(options){
    this.options = options;
  } 
+
+ /* This should build the tree */
+ buildTree(){
+
+ }
  
- buildTree(){},
- pushToTree(){}
+ /* This should push a new node to the tree */
+ pushToTree(){
+
+ }
+
+ /* This should delete a tree */
+ deleteATree(){
+
+ }
+
 }
