b054c73003b3363f37d34bc93e8afc3f858c198b
Sampath Paravasthu <write2sampath@gmail.com>
2023-01-23 00:11:10 +0530

    added data

---
+++ data.js
@@ -0,0 +1,14 @@
+let data = {
+  'component-1': {
+    children: ['component-2', 'component-2']
+  },
+  'component-2': {
+    children: ['component-3', 'component-5']
+  },
+  'component-3': {
+    children: ['component-4', 'component-5']
+  },
+  'component-4': {
+    children: ['component-5']
+  }
+}
