8369f535d82c933ca27183856f01d71a198e0faa
Sampath Paravasthu <write2sampath@gmail.com>
2023-01-24 01:11:59 +0530

    added logic for dom manipulations

--- tree.js
+++ tree.js
@@ -1,3 +1,23 @@
+function _createTextNode(text){
+  return document.createTextNode(text);
+}
+
+function _createUlElement(){
+  return document.createElement('ul');
+}
+
+function _createListElement(text){
+  let li = document.createElement('li');
+  let code = document.createElement('code');
+  code.appendChild(_createTextNode(text))
+  li.appendChild(code);
+  return li;
+}
+
+function _appendLi(ul, li) {
+  ul.appendChild(li);
+  return ul;
+}
 
 
 class Tree {
@@ -13,6 +33,20 @@ class Tree {
   buildTree(data){
     let rootId = this.#options.id;
     let rootElement = document.querySelector(rootId);
+
+    let fragment = document.createDocumentFragment();
+
+    /* This where the document builder logic goes */
+
+    let ul = _createUlElement();
+    let li = _createListElement('this is text');
+    let b = _appendLi(ul, li);
+
+
+    /* This where the document builder logic goes */
+
+    fragment.appendChild(b);
+    rootElement.appendChild(fragment);
   }
   
   /* This should delete a tree */
