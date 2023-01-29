eeaaed02aa4a1cfcecad6e6f8c23066f084eea6f
Sampath Paravasthu <write2sampath@gmail.com>
2023-01-29 21:23:51 +0530

    removed commented code

--- data.js
+++ data.js
@@ -1,19 +1,3 @@
-// let data = {
-//   'component-1': {
-//     children: ['component-2', 'component-3']
-//   },
-//   'component-2': {
-//     children: ['component-3', 'component-5']
-//   },
-//   'component-3': {
-//     children: ['component-4', 'component-5']
-//   },
-//   'component-4': {
-//     children: ['component-5']
-//   }
-// }
-
-
 let data = [
   {
     name: 'route-1',
