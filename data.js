1cedf628723a1c61fd38f49cc648ad8ebd8d128c
Sampath Paravasthu <write2sampath@gmail.com>
2023-01-29 21:23:13 +0530

    updated json data

--- data.js
+++ data.js
@@ -1,16 +1,58 @@
-let data = {
-  'component-1': {
-    children: ['component-2', 'component-3']
+// let data = {
+//   'component-1': {
+//     children: ['component-2', 'component-3']
+//   },
+//   'component-2': {
+//     children: ['component-3', 'component-5']
+//   },
+//   'component-3': {
+//     children: ['component-4', 'component-5']
+//   },
+//   'component-4': {
+//     children: ['component-5']
+//   }
+// }
+
+
+let data = [
+  {
+    name: 'route-1',
+    children: [
+      {
+        name: 'component-1',
+        children: [
+          {
+            name: 'component-2',
+            children: []
+          }
+        ]
+      },
+      {
+        name: 'component-3',
+        children: [
+          {
+            name: 'component-4',
+            children: []
+          }
+        ]
+      }
+    ]
   },
-  'component-2': {
-    children: ['component-3', 'component-5']
-  },
-  'component-3': {
-    children: ['component-4', 'component-5']
-  },
-  'component-4': {
-    children: ['component-5']
+  {
+    name: 'route-2',
+    children: [
+      {
+        name: 'component-3',
+        children: [
+          {
+            name: 'component-4',
+            children: []
+          }
+        ]
+      }
+    ]
   }
-}
+];
+
 
 export default data;
